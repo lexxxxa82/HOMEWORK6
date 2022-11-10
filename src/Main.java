058493a81ebeaf2ff926f861e95c3be897ca3ed4
lexxxxa82 <lexxxxa.82@mail.ru>
2022-11-10 21:46:16 +0800

    УБРАЛ ПЕРЕМЕННУЮ И СКОБКИ

--- src/Main.java
+++ src/Main.java
@@ -10,70 +10,71 @@ public class Main {
             month++;
             System.out.println(" месяц " + month + " сумма накоплений равна " + total + " рублей");
         }
-        System.out.println("задача № 2");{
+        System.out.println("задача № 2");
+        {
             int i = 0;
             while (i < 10) {
                 i++;
                 System.out.print(i + " ");
             }
         }
-        System.out.println(" ");{
+        System.out.println(" ");
+        {
             int i = 11;
             while (i > 1) {
                 i--;
                 System.out.print(i + " ");
             }
-                System.out.println();
+            System.out.println();
         }
-        System.out.println("задача № 3");{
-            int population = 12_000_000;
-            int fertility = 17;
-            int mortality = 8;
-            int year = 1;
-            int populationPerYear = fertility - mortality;
-            for (; year <= 10; year++) {
-                population = population + population / 1000 * populationPerYear;
-                System.out.println(" Год " + year + " численность населения составляет " + population);
-            }
+        System.out.println("задача № 3");
+        int population = 12_000_000;
+        int fertility = 17;
+        int mortality = 8;
+        int populationPerYear = fertility - mortality;
+        int year;
+        for (year = 1; year <= 10; year++) {
+            population = population + population / 1000 * populationPerYear;
+            System.out.println(" Год " + year + " численность населения составляет " + population);
         }
-        System.out.println("задача № 4");{
-            double contribution = 15000;
-            double percent = 1.07;
-            int i = 1;
-            for (; contribution <= 120000; i++) {
+        System.out.println("задача № 4");
+        {
+        double contribution = 15000;
+        double percent = 1.07;
+        int i = 1 ;
+        for (; contribution <= 120000; i++) {
                 contribution = contribution * percent;
                 System.out.println(" месяц " + i + " сумма накоплений равна " + contribution + " рублей");
             }
         }
-        System.out.println("задача № 5");{
+        System.out.println("задача № 5");
+        {
             double contribution = 15000;
             double percent = 1.07;
-            int i = 1;
+            int i = 1 ;
             for (; contribution <= 120000; i++) {
                 contribution = contribution * percent;
-                if (i % 6 == 0) {
+                if (i % 6 == 0)
                     System.out.println(" месяц " + i + " сумма накоплений равна " + contribution + " рублей");
-                }
             }
         }
-        System.out.println("задача № 6");{
+        System.out.println("задача № 6");
+        {
             double contribution = 15000;
             double percent = 1.07;
-            int i = 1;
-            for (; i <= 108; i++) {
+            int i ;
+            for (i = 1; i <= 108; i++) {
                 contribution = contribution * percent;
-                if (i % 6 == 0) {
+                if (i % 6 == 0)
                     System.out.println(" месяц " + i + " сумма накоплений равна " + contribution + " рублей");
-                }
             }
         }
         System.out.println("задача № 7");
-            int dayFriday = 3;
-            int daysInAMonth = 31;// наверное эту переменную можно не объявлять т к она не используется
-            for (; dayFriday < 31; dayFriday += 7)
-                System.out.println("Сегодня пятница," + dayFriday + "-е число. Необходимо подготовить отчет");
-        System.out.println("задача № 8");{
-            int year = 0;
+        int dayFriday = 3;
+        for (; dayFriday < 31; dayFriday += 7)
+            System.out.println("Сегодня пятница," + dayFriday + "-е число. Необходимо подготовить отчет");
+        System.out.println("задача № 8");
+            year = 0;
             int ourYear = 2022;
             int beforeOurYear = ourYear - 200;
             int afterOurYear = ourYear + 100;
@@ -82,7 +83,6 @@ public class Main {
                 if (year >= beforeOurYear && year <= afterOurYear)
                     System.out.println(year);
             }
-        }
         System.out.println("задача № 9");
         int y = 1;
         for (; y <= 10; y++) {
